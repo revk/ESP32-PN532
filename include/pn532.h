3ef7a450a19f5a12be8fca8c1d247c0c36e9d1c3
RevK <github@e.gg>
2025-11-09 16:05:28 +0000

    OK now ready for target functions

--- include/pn532.h
+++ include/pn532.h
@@ -85,20 +85,22 @@ const char *pn532_err_to_name(pn532_err_t);
 int pn532_tx(pn532_t *, uint8_t cmd, int, uint8_t *, int, uint8_t *);   // Send data to PN532 (up to two blocks) return 0 or negative for error. Starts byte after cmd
 int pn532_ready(pn532_t * p);   // For async command handling: >0 if response ready, 0 if not, -ve if error (e.g. no response expected)
 int pn532_rx(pn532_t *, int, uint8_t *, int, uint8_t *, int ms);        // Recv data from PN532, (in to up to two blocks) return total length or -ve for error, checks res=cmd+1 and returns from byte after
-uint8_t *pn532_nfcid(pn532_t *, char text[21]); // Get NFCID (first byte is len of following)
-uint8_t *pn532_ats(pn532_t *);  // Get ATS (first byte is len of following - note, not as received were it is len inc the length byte)
+				
+int pn532_write_GPIO(pn532_t * p, uint8_t value);       // (P72/P71 in top bits, P35-30 in rest)
+int pn532_read_GPIO(pn532_t * p);       // P72/P71 in top bits, P35-30 in rest)
 
 // Card access function - sends to card starting CMD byte, and receives reply in to same buffer, starting status byte, returns len
 int pn532_dx(void *, unsigned int len, uint8_t * data, unsigned int max, const char **errstr);
 
-// Higher level useful PN532 functions
+// Higher level initiator functions
+uint8_t *pn532_nfcid(pn532_t *, char text[21]); // Get NFCID (first byte is len of following)
+uint8_t *pn532_ats(pn532_t *);  // Get ATS (first byte is len of following - note, not as received were it is len inc the length byte)
 int pn532_deselect(pn532_t * p, uint8_t n);     // Send deselect ID 1 or 2
 int pn532_release(pn532_t * p, uint8_t n);      // Send release ID 1 or 2
-int pn532_write_GPIO(pn532_t * p, uint8_t value);       // (P72/P71 in top bits, P35-30 in rest)
-int pn532_read_GPIO(pn532_t * p);       // P72/P71 in top bits, P35-30 in rest)
 int pn532_ILPT_Send(pn532_t * p);       // Async InListPassiveTarget - used pn532_ready to check when to do pn532_Cards
 int pn532_Cards(pn532_t * p);   // How many cards present (does pn532_ILPT_Send if needed)
 int pn532_Present(pn532_t * p); // Check if present still
-
+				
+// Higher level target functions
 
 #endif
